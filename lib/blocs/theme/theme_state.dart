3dd4ecea750b81b7bab839ac9d2615fd6492f887
jsmile <jsmile@naver.com>
2023-08-09 22:59:07 +0900

    hydrate를 위해 State와 Bloc에 json initialization 추가

--- lib/blocs/theme/theme_state.dart
+++ lib/blocs/theme/theme_state.dart
@@ -2,7 +2,11 @@ part of 'theme_bloc.dart';
 
 enum AppTheme {
   light,
-  dark,
+  dark;
+
+  String toMap() => name;
+
+  static AppTheme fromMap(String json) => values.byName(json);
 }
 
 class ThemeState extends Equatable {
@@ -25,4 +29,21 @@ class ThemeState extends Equatable {
 
   @override
   String toString() => 'ThemeState(appTheme: $appTheme)';
+
+  Map<String, dynamic> toMap() {
+    return {
+      'appTheme': appTheme.toMap(),
+    };
+  }
+
+  factory ThemeState.fromMap(Map<String, dynamic> map) {
+    return ThemeState(
+      appTheme: AppTheme.fromMap(map['appTheme']),
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory ThemeState.fromJson(String source) =>
+      ThemeState.fromMap(json.decode(source));
 }
